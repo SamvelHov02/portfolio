c032224b7510ef3100026d96cd11f2e6ed3c5340
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-08-28 23:46:46 +0200

    docs : Update about section

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 export default {
-    about : ["Hi, my name is Samvel Hovhannisyan and I'm a Master's student in computer science at KTH. I'm a learner and a curious soul who always strives to understand technologies, as the saying goes 'You don't know something unless you are able to explain it'. I ..."],
+    about : 
+    "Hi! my name is Samvel and I'm a computer science student at KTH, currently working on finishing my Master's degree. I'm a software engineer hopeful, I love working with software, understanding technical systems as well as possible and problem solving. I think software engineering is more than just coding, a strength of mine is to think about a problem and a solution before jumping into coding. \n\n I'm mainly Python programming, familiar with many libraries e.g. Numpy, Pytorch, Pandas, but also with Backend Engineering libraries such as FastAPI. Other than Python I also love front end engineering and have compentencies in React, HTML, CSS. Lastly I also hold an affinity for Go, it's simplistic syntax combined with the performance benefits makes it a real joy to work with in my opinion. \n\nWhen not programming I enjoy many other hobbies, playing games with friends, baking or cooking a recipe that caught my interest while during the week, reading or just slouching on the couch while watching a movie or anime.",
     projects : [
         {
             name : "Date Translator",
